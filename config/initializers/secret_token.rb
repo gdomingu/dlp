1d2e4c4007f50c211b0ed65e8c35da9e4eb1e207
Gabe <gabedominguez@outlook.com>
2013-07-13 09:48:20 -0700

    first commit changed database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dlp::Application.config.secret_token = '21f7254c7ad43c5a51a89086abb089b0e6d4383658f5d080e2db1024595c42f1e884dd236cdedf32e616a7f1df2c62d78c558e605e27d7a191dac5d6cff5446c'
