a3c31a4943e11b5a85e2549b43f0f9fe37a4621a
Gabe <gabedominguez@outlook.com>
2013-07-15 19:30:04 -0700

    rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Dlp::Application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
+  ActiveAdmin.routes(self)
 
   root :to => 'welcome#index'
 
