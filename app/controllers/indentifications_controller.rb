803e2ee0aedfdd09a16491e25f79065ff9a4afb5
Gabe <gabedominguez@outlook.com>
2013-07-16 21:59:51 -0700

    deleted

--- app/controllers/indentifications_controller.rb
+++
@@ -1,2 +0,0 @@
-class IndentificationsController < InheritedResources::Base
-end
--- db/migrate/20130716205654_create_indentifications.rb
+++
@@ -1,9 +0,0 @@
-class CreateIndentifications < ActiveRecord::Migration
-  def change
-    create_table :indentifications do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
