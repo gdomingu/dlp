a3f6f7e33096b0b88a22899169cce8980bbac459
Gabe <gabedominguez@outlook.com>
2013-07-19 16:22:48 -0700

    worked on user auth, sign in sign out

--- app/controllers/worker_profiles_controller.rb
+++ app/controllers/worker_profiles_controller.rb
@@ -6,6 +6,8 @@ class WorkerProfilesController < InheritedResources::Base
   def create
      @worker = WorkerProfile.new(params[:worker_profile])
     if @worker.save
+      @user = User.find_by_id(current_user.id)
+      @user.update_attributes(:profile_id => @worker.id, :profile_type => @worker)
       redirect_to root_path
     else
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :timeoutable
-    attr_accessible :email, :password, :password_confirmation, :remember_me
+    attr_accessible :email, :password, :password_confirmation, :remember_me, :profile_id, :profile_type
   belongs_to :profile, polymorphic: true
   has_many :photos
 end
