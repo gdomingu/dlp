f55104b933385b2dd992663df1de326b0c38d620
Gabe <gabedominguez@outlook.com>
2013-07-22 09:29:47 -0700

    added more pictures and updated admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,12 +4,27 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    end
+    section "New Members" do
+        table_for WorkerProfile.order("created_at desc").limit(5) do
+          column :first_name do |worker|
+            link_to worker.first_name, [:admin, worker]
+          end
+          column :last_name
+          column :gender
+          column :home_phone
+          column :address
+          column :flyered, :as => "Last Flyered"
+          column :created_at
+          column :jobs do |job|
+              if :jobs
+                link_to "Job History", admin_job_path(job.id)
+              end
+        end
+        strong { link_to "View All Workers", admin_worker_profiles_path }
       end
     end
-
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
